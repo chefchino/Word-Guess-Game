2784cfd33b0ccc179fd03215791092904856f379
Ciprien Bossut <chefchino@live.com>
2019-09-23 14:26:35 -0400

    stuck on getElementById

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,25 @@
+
 // create an array of words 
-const word = [  "nirvana",  "nwa", "tupac", "korn","deftones",  "moby",];
+const word = ["nirvana", "nwa", "tupac", "korn", "deftones", "moby",];
 // create underscores to hold place of words until guess\
-let randNum = Math.floor(Math.random() * word.length);
-let choosenWord = word[randNum];
-let underscore = [];
+var wins = 0;
+var guessLeft = 12;
+let randNum;
+let underScore = [];
 let rightArray = [];
 let wrongArray = [];
 
-console.log(choosenWord);
+function startGame(){
+    randNum = word[Math.floor(Math.random() * word.length)];
 
-let makeUnderScore = () => {
-  for  (i = 0; i < choosenWord.length; i++) {
-    underscore.push("_");
-  }
- return underscore;
+    for (i = 0; i < randNum.length; i++) 
+    {
+        underScore.push(" _ ");       
+    }
+    console.log(underScore);
+    document.getElementById("wordsG").textContent = underScore;
+
 }
-console.log(makeUnderScore());
-// user guess
-document.addEventListener("keypress", (event) => {
-console.log(event);
-let keyword = String.fromCharCode(event.keyCode);
-    if (choosenWord.indexOf(keyword) > -1) {
-        rightArray.push(keyword);
-        underscore[choosenWord.indexOf(keyword)] = keyword;
-        console.log(rightArray);
-    } else wrongArray.push(keyword);
-        console.log(wrongArray);
-});
-// check if right
-// right array
-// wrong array
-// keep score
+
+startGame();
+
