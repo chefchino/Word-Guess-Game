f07996cf4a5452aac0327f16441f92a0e308775b
Ciprien Bossut <chefchino@live.com>
2019-09-26 18:39:24 -0400

    words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,16 +11,17 @@ let underScore = [];
 let rightArray = [];
 let wrongArray = [];
 let blank;
-let correct =[];
+let correct ="";
 
 function startGame(){
     choosen = word[Math.floor(Math.random() * word.length)];
         console.log(choosen);
     for (i = 0; i < choosen.length; i++) 
     {
+        // correct[i] = choosen.charAt(i);
         underScore[i] = " _ ";      
     }
-    let remainingLetters = choosen.length;
+    // let remainingLetters = choosen.length;
     console.log(underScore);
     
     document.getElementById("wordsG").innerHTML = underScore; underScore.join("");
@@ -28,38 +29,73 @@ function startGame(){
 }
 
 document.addEventListener("keypress", (event) => {
+    // changes = 0;
+    
     let keyword = String.fromCharCode(event.keyCode);
-    changes = 0;
+    console.log("keyword", keyword);
         
-    // for ( j = 0; j< choosen.length; j++) {
-    //     if (choosen[j] === keyword){
-    //         rightArray[j] = keyword;
-            // rightArray[j];
-        // }
-        // console.log(rightArray[j]);
-    // }
-    if(choosen.indexOf(keyword) > -1) {
-        rightArray.push(keyword); 
-        console.log(rightArray);
-    }
-    else { wrongArray.push(keyword);
-        if(changes < 1){
-            guessLeft -=1;
-            document.getElementById("guess1").innerHTML = guessLeft;
+    // Looping through the chosen word
+    for ( j = 0; j< choosen.length; j++) {
+        rightArray[j] = choosen.charAt(j);
+        if(choosen.charAt(j) == keyword) {
+            underScore[j] = keyword;
+            console.log("underscore", underScore);
         }
+        // } else {
+        //     guessLeft--; 
+        //     wrongArray.push(keyword);
+        
+        // }
+        document.getElementById("wordsG").innerHTML = underScore; 
+        // changes++;
+    }
+        if(!choosen.includes(keyword)) {
+            guessLeft--;
+            wrongArray.push(keyword);
+            console.log("wrongArray", wrongArray);
+            console.log("guessed", guessLeft);
+        }
+        // if (choosen[j] === keyword){
+        //     console.log(j);
+        //     underScore[j] = keyword;
+        //     correct[j] += underScore[j];
+        //     underScore[j]; 
+            
+        // }
+        
+        
+        // if (changes < 1){
+        //     guessLeft--;
+        //     document.getElementById("guess1").innerHTML = guessLeft;
 
-    }
-    if (guessLeft === 0) {
-        alert("You Lose!!!!");
-    }
-        console.log(wrongArray);
+        // }
+              
+        // console.log("underScore", underScore[j]);
+        // console.log("wrong", wrongArray[j]);
+    
+
+    // if(choosen.indexOf(keyword) > -1) {
+    //     rightArray.push(keyword); 
+    //     console.log(rightArray);
+    // }
+    
+    //     if(changes < 1){
+    //         guessLeft -=1;
+    //         
+    //     }
+
+    // // }
+    // if (guessLeft === 0) {
+    //     alert("You Lose!!!!");
+    // }
+        // console.log(wrongArray);
      
-    document.getElementById("wordsG").innerHTML = rightArray; rightArray.join("");
+
+    // rightArray.join("");
 
     
-    document.getElementById("lettersGuess").innerHTML = wrongArray;
+    // document.getElementById("lettersGuess").innerHTML = wrongArray;
     });
 
 
 startGame();
-
---
+++ assets/javascript/thisGame.js
@@ -0,0 +1,62 @@
+
+// create an array of words 
+const word = ["nirvana", "nwa", "tupac", "korn", "deftones", "moby",];
+// create underscores to hold place of words until guess\
+var wins = 0;
+var guessLeft = 12;
+let choosen;
+let underScore = [];
+let rightArray = [];
+let wrongArray = [];
+let blank;
+let correct =[];
+
+function startGame(){
+    choosen = word[Math.floor(Math.random() * word.length)];
+        console.log(choosen);
+    for (i = 0; i < choosen.length; i++) 
+    {
+        underScore[i] = " _ ";      
+    }
+    let remainingLetters = choosen.length;
+    console.log(underScore);
+    
+    document.getElementById("wordsG").innerHTML = underScore; underScore.join("");
+  
+}
+
+document.addEventListener("keypress", (event) => {
+    let keyword = String.fromCharCode(event.keyCode);
+    changes = 0;
+        
+    // for ( j = 0; j< choosen.length; j++) {
+    //     if (choosen[j] === keyword){
+    //         rightArray[j] = keyword;
+            // rightArray[j];
+        // }
+        // console.log(rightArray[j]);
+    // }
+    if(choosen.indexOf(keyword) > -1) {
+        rightArray.push(keyword); 
+        console.log(rightArray);
+    }
+    else { wrongArray.push(keyword);
+        if(changes < 1){
+            guessLeft -=1;
+            document.getElementById("guess1").innerHTML = guessLeft;
+        }
+
+    }
+    if (guessLeft === 0) {
+        alert("You Lose!!!!");
+    }
+        console.log(wrongArray);
+     
+    document.getElementById("wordsG").innerHTML = rightArray; rightArray.join("");
+
+    
+    document.getElementById("lettersGuess").innerHTML = wrongArray;
+    });
+
+
+startGame();
